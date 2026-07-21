aa0929eef9ee00f256f284ebca20193f19460d44
pedrowowbr <edupedrobraga@gmail.com>
2026-07-21 13:51:07 -0300

    Feat: Apresentando os dados com streamlit

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ import sqlalchemy
 # Própria máquina
 engine = sqlalchemy.create_engine("sqlite:///database.db")
 
+with open("query_inteligente.sql") as query_file:
+    query = query_file.read()
+
+try:
+    df_stats = pd.read_sql(query, engine)
+
+except Exception as err:
+    print(err)
+    df_stats = pd.DataFrame()
+
 st.set_page_config(page_title="Lista Inteligente")
 
 st.markdown("# Lista de compras inteligente!")
-st.markdown("## Importar Histórico")
+if df_stats.empty:
+    st.warning("Não há dados históricos suficientes. Registre mais compras!")
+else:
+    st.dataframe(df_stats)
 
+st.markdown("## Importar Histórico")
 open_file = st.file_uploader("Entre com um arquivo histórico", type="csv")
 
 if open_file:
