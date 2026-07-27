03626dff599d486937df461111679c7d2f9e1587
pedrowowbr <edupedrobraga@gmail.com>
2026-07-27 00:41:38 -0300

    Feature: Incluindo abas e realizando melhorias no código

--- main.py
+++ main.py
@@ -36,9 +36,21 @@ def process_nf(prompt, resposta_template, produtos, img_file):
     return df
 
 
+def get_produtos(engine):
+    try:
+        query = """SELECT DISTINCT produto FROM compras ORDER BY 1"""
+        df = pd.read_sql(query, engine)
+        return df["produto"].sort_values().tolist()
+    except Exception as err:
+        print(err)
+        return []
+
+
 st.set_page_config(page_title="Lista Inteligente")
 
 st.markdown("# Lista de compras inteligente!")
+
+produtos = get_produtos(engine)
 
 try:
     col, _ = st.columns(2)
@@ -48,61 +60,71 @@ try:
     df_stats = pd.read_sql(query, engine)
     df_stats["comprar"] = df_stats["dias_desde_ultima_compra"] + \
         numero_dias_adiante > df_stats["avg_diff_dias_entre_compras"]
-    df_compra = df_stats[df_stats["comprar"]]
+    df_comprar = df_stats[df_stats["comprar"]]
 
 except Exception as err:
-    print(err)
-    df_compra = pd.DataFrame()
+    st.exception(err)
+    df_comprar = pd.DataFrame()
+    df_stats = pd.DataFrame()
 
 if df_stats.empty:
     st.warning("Não há dados históricos suficientes. Registre mais compras!")
+elif df_comprar.empty:
+    st.success(
+        f"Não há nada a ser comprado considerando {numero_dias_adiante} dias.")
 else:
-    st.dataframe(df_compra)
+    st.dataframe(df_comprar)
 
-st.markdown("## Adicionar Compra")
+st.markdown("## Adicionar Compras")
 
-produtos = df_compra["produto"].unique().tolist()
-produtos.sort()
-produto = st.selectbox("Produto", options=[
-                       "Novo Produto"] + produtos)
+tab_produto, tab_historico, tab_nf = st.tabs(
+    ["Produto", "Histórico", "Nota Fiscal"])
 
-if produto == "Novo Produto":
-    produto_novo = st.text_input("Inserir novo Produto")
-    produto = produto_novo
+with tab_produto:
 
-valor = st.number_input("Valor", min_value=0.01)
+    st.markdown("### Registrar Produto")
+    produto = st.selectbox("Produto", options=[
+        "Novo Produto"] + produtos)
 
-if st.button("Registrar Compra"):
-    data = {
-        "dt_compra": datetime.datetime.now().strftime("%Y-%m-%d"),
-        "produto": produto.title(),
-        "valor_produto": valor,
-    }
+    if produto == "Novo Produto":
+        produto_novo = st.text_input("Inserir novo Produto")
+        produto = produto_novo
 
-    df_insert = pd.DataFrame([data])
-    df_insert.to_sql("compras", engine, if_exists="append", index=False)
-    st.success("Compra do produto registrada com sucesso!")
+    valor = st.number_input("Valor", min_value=0.01)
 
-st.markdown("## Importar Histórico")
-open_file = st.file_uploader("Entre com um arquivo histórico", type="csv")
+    if st.button("Registrar Produto"):
+        data = {
+            "dt_compra": datetime.datetime.now().strftime("%Y-%m-%d"),
+            "produto": produto.title(),
+            "valor_produto": valor,
+        }
 
-if open_file:
-    df = pd.read_csv(open_file)
-    df = st.data_editor(df)
+        df_insert = pd.DataFrame([data])
+        df_insert.to_sql("compras", engine, if_exists="append", index=False)
+        st.success("Compra do produto registrada com sucesso!")
 
-    if st.button("Registrar Dados!"):
-        df.to_sql("compras", engine, if_exists="append", index=False)
-        st.success("Dados registrados com sucesso!")
+with tab_historico:
+    st.markdown("### Importar Histórico")
+    open_file = st.file_uploader("Entre com um arquivo histórico", type="csv")
 
-st.markdown("## Importar Nota Fiscal")
+    if open_file:
+        df = pd.read_csv(open_file)
+        df = st.data_editor(df)
 
-open_img = st.file_uploader(
-    "Entre com um arquivo de Nota Fiscal", type=["png", "jpeg"])
+        if st.button("Registrar Dados!"):
+            df.to_sql("compras", engine, if_exists="append", index=False)
+            st.success("Dados registrados com sucesso!")
 
-if open_img:
-    df = process_nf(prompt=prompt, resposta_template=resposta,
-                    produtos=produtos, img_file=open_img)
-    df = st.data_editor(df)
-    if st.button("Registrar Dados!"):
-        df.to_sql("compras", engine, if_exists="append", index=False)
-        st.success("Dados registrados com sucesso!")
+with tab_nf:
+    st.markdown("### Importar Nota Fiscal")
+
+    open_img = st.file_uploader(
+        "Entre com um arquivo de Nota Fiscal", type=["png", "jpeg"])
+
+    if open_img:
+        df = process_nf(prompt=prompt, resposta_template=resposta,
+                        produtos=produtos, img_file=open_img)
+        df = st.data_editor(df)
+        if st.button("Registrar Dados!"):
+            df.to_sql("compras", engine, if_exists="append", index=False)
+            st.success("Dados registrados com sucesso!")
