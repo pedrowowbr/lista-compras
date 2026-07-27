72f479fbab53173fb77683fd96cfe4043442d437
pedrowowbr <edupedrobraga@gmail.com>
2026-07-27 00:40:34 -0300

    Fix: Alterando query para agrupar por data compra e produto

--- query_inteligente.sql
+++ query_inteligente.sql
@@ -1,10 +1,18 @@
-WITH TB_LAG AS(
+WITH tb_compras AS(
+    SELECT dt_compra,
+        produto,
+        avg(valor_produto) as valor_produto
+    FROM compras
+    GROUP BY dt_compra,
+        produto
+),
+TB_LAG AS(
     SELECT *,
         LAG(dt_compra) OVER(
             PARTITION BY produto
             ORDER BY dt_compra
         ) AS dt_compra_anterior
-    FROM compras
+    FROM tb_compras
     ORDER BY produto,
         dt_compra
 ),
@@ -22,9 +30,14 @@ TB_STATS_PRODUTO AS (
         avg(valor_produto) AS media_valor_produto
     FROM compras
     GROUP BY produto
+),
+tb_final AS(
+    SELECT T1.*,
+        T2.avg_diff_dias_entre_compras,
+        julianday('now') - julianday(dt_ultima_compra) as dias_desde_ultima_compra
+    FROM TB_STATS_PRODUTO as T1
+        LEFT JOIN tb_avg as T2 ON T1.produto = T2.produto
 )
-SELECT T1.*,
-    T2.avg_diff_dias_entre_compras,
-    julianday('now') - julianday(dt_ultima_compra) as dias_desde_ultima_compra
-FROM TB_STATS_PRODUTO as T1
-    LEFT JOIN tb_avg as T2 ON T1.produto = T2.produto
+SELECT *
+FROM tb_final
+ORDER BY dt_ultima_compra
